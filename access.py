7ea5481c5ca4590f13849d9b5b760be794aad422
jimmytana <Kubwimanajimmy96@gmail.com>
2025-03-24 23:55:05 +1400

    finding the sum of coins

--- access.py
+++ access.py
@@ -1,2 +1,7 @@
-a=[x for x in range (100)if x%2==0 ] 
-print(a)
+studs = { 50:7, 100: 14}
+sum = 0
+for i in studs.keys():
+    sum = sum + (studs[i]*i)
+print(sum)
+# a = [x for x in range (100)if x%2==0 ] 
+# print(a)
